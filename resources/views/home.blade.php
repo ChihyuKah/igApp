a8ce7984d1098ecbeb143c456913c6b916b6a940
Chihyu <chihyu10@gmail.com>
2020-05-26 17:03:00 +0200

    working in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+            <div class="col-3 p5">
+                <img
+                    src='https://scontent-bru2-1.cdninstagram.com/v/t51.2885-19/s150x150/60767054_2420350861517535_1223563301330354176_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_ohc=J61gRfOkfYQAX-hqp6V&oh=a7ee00bb4e705b1eb7e5a8a0b1df0b14&oe=5EF84E3A'
+                    class="rounded-circle">
+            </div>
+            <div class="col-9 pt-5">
+                <div><h1>Chihyu Kah</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>4</strong> posts</div>
+                    <div class="pr-5"><strong>24k</strong> followers</div>
+                    <div class="pr-5"><strong>242</strong> following</div>
                 </div>
             </div>
+
         </div>
     </div>
-</div>
 @endsection
