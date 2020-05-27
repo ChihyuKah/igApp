df9094e3092c0d4172ea7596540c9028ac672eee
Chihyu <chihyu10@gmail.com>
2020-05-27 10:10:46 +0200

    adding front-end layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-3 p5">
+            <div class="col-3 p-5">
                 <img
                     src='https://scontent-bru2-1.cdninstagram.com/v/t51.2885-19/s150x150/60767054_2420350861517535_1223563301330354176_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_ohc=J61gRfOkfYQAX-hqp6V&oh=a7ee00bb4e705b1eb7e5a8a0b1df0b14&oe=5EF84E3A'
                     class="rounded-circle">
@@ -16,8 +16,26 @@
                     <div class="pr-5"><strong>24k</strong> followers</div>
                     <div class="pr-5"><strong>242</strong> following</div>
                 </div>
+                <div class="pt-4 font-weight-bold">Bio name</div>
+                <div>Bio text</div>
+                <div><a href="#">www.testlink.com</a></div>
             </div>
-
         </div>
-    </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img
+                    src="https://scontent-bru2-1.cdninstagram.com/v/t51.2885-15/e35/s150x150/100881924_2522556044663611_4273294490229493048_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_cat=1&_nc_ohc=WuiwJK2eONcAX-izOlt&oh=2ff34500939fe4904d66330e21fd04f9&oe=5EF66AA3"
+                    alt="#" class="w-100">
+            </div>
+            <div class="col-4">
+                <img
+                    src="https://scontent-bru2-1.cdninstagram.com/v/t51.2885-15/e35/s150x150/100881924_2522556044663611_4273294490229493048_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_cat=1&_nc_ohc=WuiwJK2eONcAX-izOlt&oh=2ff34500939fe4904d66330e21fd04f9&oe=5EF66AA3"
+                    alt="#" class="w-100">
+            </div>
+            <div class="col-4">
+                <img
+                    src="https://scontent-bru2-1.cdninstagram.com/v/t51.2885-15/e35/s150x150/100881924_2522556044663611_4273294490229493048_n.jpg?_nc_ht=scontent-bru2-1.cdninstagram.com&_nc_cat=1&_nc_ohc=WuiwJK2eONcAX-izOlt&oh=2ff34500939fe4904d66330e21fd04f9&oe=5EF66AA3"
+                    alt="#" class="w-100">
+            </div>
+        </div>
 @endsection
